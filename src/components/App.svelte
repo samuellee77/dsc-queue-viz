8502066a087df07f62b85786c700573b7cd6933e
samuellee77 <hsl023@ucsd.edu>
2024-05-22 15:12:01 -0700

    add more wording

--- src/components/App.svelte
+++ src/components/App.svelte
@@ -19,20 +19,28 @@
 <main>
   <h1>Queue Visualization</h1>
   <h2> Introduction </h2>
-  <p> Queue is a fundamental concept in computer science used for storing and managing data in a specific order. It follows the principle of <b>“First in, First out” (FIFO)</b>, where the first element added to the queue is the first one to be removed. Queues are commonly used in various algorithms and applications for their simplicity and efficiency in managing data flow.</p>
-  <p> Source: <a href='https://www.geeksforgeeks.org/queue-data-structure/'> GeeksforGeeks</a></p>
+  <p> 
+    Queue is a fundamental concept in computer science used for storing and managing data in a specific order. It follows the principle of <b>“First in, First out” (FIFO)</b>, where the first element added to the queue is the first one to be removed. Queues are commonly used in various algorithms and applications for their simplicity and efficiency in managing data flow.
+  </p>
+  <p> Source: 
+    <a href='https://www.geeksforgeeks.org/queue-data-structure/'> GeeksforGeeks</a>
+  </p>
   <h2> Sample Data </h2>
   <p> Below is a list of sample data that is used for a office hour system for students. Each data entry has:
     <li> PID </li>
     <li> name </li>
     <li> timestamp </li>
+  </p>
   <ul>
     {#each data as item}
-      <li>PID: {item.pid} <br>
+      <li>
+        PID: {item.pid} <br>
         Name: {item.name} <br>
-        Timestamp: {item.timestamp}</li>
+        Timestamp: {item.timestamp}
+      </li>
     {/each}
   </ul>
+  <h2> Visualization </h2>
 </main>
 {/if}
 
