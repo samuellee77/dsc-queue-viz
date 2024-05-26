a5b1246d37fdac76d2f600112db5bc45a4ed4631
samuellee77 <hsl023@ucsd.edu>
2024-05-25 23:01:08 -0700

    draw US map

--- src/components/App.svelte
+++ src/components/App.svelte
@@ -1,67 +1,66 @@
 <script>
   import { onMount } from 'svelte';
   import * as d3 from 'd3';
+  import Papa from 'papaparse';
+  import * as topojson from 'topojson-client';
 
-  let data;
+  let wageData;
   let loading = true;
-  let svgElement
+  let svgElement;
+  let us;
+  let path = d3.geoPath();
+  const setYear = 2000;
+
   onMount(async () => {
-    const jsonData = await fetch('sample_data.json').then((response) => response.json());
-    console.log(jsonData);
-    data = jsonData;
-    loading = false;
-  });
-  function generateQueue() {  
-    const svg = d3.select(svgElement);
-    const width = 800;
-    const height = 400;
-    svg.append('rect')
-      .attr('width', width)
-      .attr('height', height)
-      .attr('fill', 'lightblue');
-    const rects = [
-      { x: 100, y: 50 },
-      { x: 200, y: 50 },
-      { x: 300, y: 50 },
-      { x: 400, y: 50 },
-      { x: 500, y: 50 }
-    ];
-    rects.forEach((rect) => {
-      svg.append('rect')
-        .attr('width', 100)
-        .attr('height', 100)
-        .attr('x', rect.x)
-        .attr('y', rect.y)
-        .attr('fill', 'white');
-      svg.append('line')
-        .attr('x1', rect.x)
-        .attr('y1', rect.y)
-        .attr('x2', rect.x)
-        .attr('y2', rect.y + 100)
-        .attr('stroke', 'black')
-        .attr('stroke-width', 3);
-      svg.append('line')
-        .attr('x1', rect.x)
-        .attr('y1', rect.y)
-        .attr('x2', rect.x + 100)
-        .attr('y2', rect.y)
-        .attr('stroke', 'black')
-        .attr('stroke-width', 3);
-      svg.append('line')
-        .attr('x1', rect.x)
-        .attr('y1', rect.y + 100)
-        .attr('x2', rect.x + 100)
-        .attr('y2', rect.y + 100)
-        .attr('stroke', 'black')
-        .attr('stroke-width', 3);
+    // load minimum wage data
+    const response = await fetch('data.csv');
+    const csvData = await response.text();
+
+    // change datatypes
+    wageData = Papa.parse(csvData, { header: true }).data.map((d) => {
+      return {
+        year: +d.year,
+        state: d.state,
+        state_minimum_wage: +d.state_minimum_wage,
+        state_minimum_wage_2020_dollars: +d.state_minimum_wage_2020_dollars,
+        federal_minimum_wage: +d.federal_minimum_wage,
+        federal_minimum_wage_2020_dollars: +d.federal_minimum_wage_2020_dollars,
+        effective_minimum_wage: +d.effective_minimum_wage,
+        effective_minimum_wage_2020_dollars: +d.effective_minimum_wage_2020_dollars,
+        cpi_average: +d.cpi_average
+      };
     });
-    svg.append('line')
-      .attr('x1', rects[rects.length - 1].x + 100)
-      .attr('y1', rects[rects.length - 1].y)
-      .attr('x2', rects[rects.length - 1].x + 100)
-      .attr('y2', rects[rects.length - 1].y + 100)
-      .attr('stroke', 'black')
-      .attr('stroke-width', 3);
+    
+    // load US map data
+    const usResponse = await fetch('counties-albers-10m.json');
+    const usData = await usResponse.json();
+    us = topojson.feature(usData, usData.objects.states).features;
+    loading = false;
+
+  });
+
+  function filterYear(year) {
+    return wageData.filter((d) => d.year === year);
+  }
+
+  function getColor(state) {
+    const stateName = state.properties.name;
+    const stateData = filterYear(setYear).find((d) => d.state === stateName);
+    if (stateData) {
+      const stateMinimumWage = stateData.effective_minimum_wage;
+      return colorScale(setYear)(stateMinimumWage);
+    }
+    return 'white';
+  }
+
+  function colorScale(year) {
+    const minWageValues = filterYear(year).map((d) => d.effective_minimum_wage);
+    const minWageMin = d3.min(minWageValues);
+    const minWageMax = d3.max(minWageValues);
+    return d3.scaleLinear()
+      .domain([minWageMin, minWageMax])
+      // colors to be determined
+      .range(['#cdebf7', 'blue']);
   }
 </script>
 
@@ -69,37 +68,18 @@
   <h2>Loading...</h2>
 {:else}
 <main>
-  <h1>Queue Visualization</h1>
-  <h2> Introduction </h2>
-  <p> 
-    Queue is a fundamental concept in computer science used for storing and managing data in a specific order. It follows the principle of <b>“First in, First out” (FIFO)</b>, where the first element added to the queue is the first one to be removed. Queues are commonly used in various algorithms and applications for their simplicity and efficiency in managing data flow.
-  </p>
-  <p> Source: 
-    <a href='https://www.geeksforgeeks.org/queue-data-structure/' target='_blank'> GeeksforGeeks</a>
-  </p>
-  <h2> Sample Data </h2>
-  <p> Below is a list of sample data that is used for a office hour system for students. Each data entry has:
-    <li> PID </li>
-    <li> name </li>
-    <li> timestamp </li>
-  </p>
-  <ul>
-    {#each data as item}
-      <li>
-        PID: {item.pid} <br>
-        Name: {item.name} <br>
-        Timestamp: {item.timestamp}
-      </li>
+  <svg width="960" height="600">
+    {#each us as state (state.id)}
+      <path d={path(state)}></path>
+      <path d={path(state)} fill={getColor(state)}></path>
     {/each}
-  </ul>
-  <h2> Visualization </h2>
-  <div> 
-    <button on:click={generateQueue}>Generate Blank Queue</button>
-    <svg bind:this={svgElement} width="800" height="400"></svg>
-    
+  </svg>
 </main>
 {/if}
 
 <style>
-  /* Write your CSS here */
+  path {
+    stroke: #000;
+    stroke-width: 1;
+  }
 </style>
