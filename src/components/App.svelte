70b5cd662fb0091af59ff25ba5ead022af1f5954
hongljasmine <129822126+hongljasmine@users.noreply.github.com>
2024-06-03 19:17:24 -0700

    Update App.svelte

--- src/components/App.svelte
+++ src/components/App.svelte
@@ -19,14 +19,28 @@
 
 {#if loading}
   <Loading/>
+
 {:else}
   <div class="container">
     <Intro />
     <LineGraph {federalWageData} {wageData} />
     <TwoLineGraphs {wageData} /> 
+    <div>
+      <h2>Alabama</h2>
+      <p>Alabama doesn't have state-mandated minimum wage laws. Instead, they adhere to the federal minimum wage set by the Fair Labor Standards Act (FLSA). </p>
+    </div>
+    <div>
+      <h2>California</h2>
+      <p>California experienced the greatest increase in minimum wage over a three-year period due to a variety of factors including legislative initiatives, economic conditions, social and political pressure, and public support.</p>
+    </div>
     <ChoroplethMap {usData} {wageData} />
+    <p>The minimum wage continues to be a contentious issue in U.S. politics. Supporters claim that higher wages are necessary to reduce poverty and stimulate economic growth by increasing consumer spending. Opponents, however, argue that substantial increases could lead to job losses and higher prices for goods and services, potentially harming small businesses.
+
+      In conclusion, the history of minimum wage legislation in the United States is a testament to the evolving understanding of fair labor standards and economic justice. From its early inception to contemporary debates, the minimum wage remains a crucial element of the nation's labor policy, reflecting broader social and economic challenges.</p>
+    
   </div>
 {/if}
+
 
 <style>
   .container {
